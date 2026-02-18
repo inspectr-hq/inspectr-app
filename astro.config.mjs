b54cd2992df03ccc127c27a3e659af5a1dc9ba2f
Tim Haselaars <timhaselaars@gmail.com>
2026-02-18 10:38:16 +0100

    feat: Make Inspectr App load relative paths

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,10 +4,11 @@ import { defineConfig } from 'astro/config';
 
 import react from '@astrojs/react';
 import tailwindcss from '@tailwindcss/vite';
+import relativeLinks from 'astro-relative-links';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()],
+  integrations: [react(), relativeLinks()],
   output: "static",
   // output: "server",
   // adapter: node({
